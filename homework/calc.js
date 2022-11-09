c43266c9d380fcf4bd3641fd2e0a504e5690c572
milena.jankovska <milena@sportsadvice24.com>
2022-11-09 17:26:55 +0100

    homework

---
+++ homework/calc.js
@@ -0,0 +1,31 @@
+let number1 = 10;
+let numebr2 = 3;
+let operation = "*";
+
+if(numebr2 == 0){
+    console.log('fatal error ---division by 0');
+}else{
+    let calc;
+    switch (operation) {
+        case "+":
+            calc = number1 + numebr2;           
+            break;
+        case "-":
+            calc = number1 - numebr2;           
+            break;
+        case "*":
+            calc = number1 * numebr2;           
+            break;
+        case "/":
+            calc = number1 / numebr2;           
+            break;
+        case "%":
+            calc = number1 % numebr2;           
+            break;
+        default:
+            calc = "---unknown operator";
+            break;
+    }
+    console.log(calc);
+}
+
