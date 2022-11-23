452e01471262197c11a663eddd9203277d4cb6fb
milena.jankovska <milena@sportsadvice24.com>
2022-11-23 16:49:55 +0100

    homework

---
+++ homework_4/hw_4.js
@@ -0,0 +1,15 @@
+function insertRow(){
+    const myTable = document.getElementById('sampleTable'); 
+    const colNumber = myTable.rows[0].cells.length;
+    const rowNumber = myTable.rows.length;
+   const newRow = myTable.insertRow();
+   for (let i = 1; i <= colNumber; i++) {
+        let cell = newRow.insertCell();
+        cell.innerText = "Row"+(rowNumber+1)+" cell"+i;
+        console.log(cell);
+   }
+
+   console.log(myTable);
+   console.log(myTable.rows);
+   console.log(myTable.rows[0]);
+}
